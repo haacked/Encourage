2cef5577bdebcf74781c147bd35adad73a131545
Jared Parsons <jaredpar@microsoft.com>
2014-07-29 07:10:32 -0700

    Example

--- EncouragePackage/IEncouragements.cs
+++ EncouragePackage/IEncouragements.cs
@@ -1,7 +1,10 @@
-﻿namespace Haack.Encourage
+﻿using System.Collections.Generic;
+namespace Haack.Encourage
 {
     public interface IEncouragements
     {
+        IEnumerable<string> AllEncouragements { get; set; }
+
         string GetRandomEncouragement();
     }
 }
