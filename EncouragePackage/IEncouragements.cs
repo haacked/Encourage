3fab4b81923403debca1d7f0072fc35b2ad3b13d
Nicollas Ricas <nicollasfernandesricas@gmail.com>
2015-02-04 22:58:27 -0300

    Added some discouragements if something is wrong
    Encouragement is nice but discouragement is even more!

--- EncouragePackage/IEncouragements.cs
+++ EncouragePackage/IEncouragements.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.Shell;
+using System.Collections.Generic;
+
 namespace Haack.Encourage
 {
     public interface IEncouragements
     {
+        IEnumerable<string> AllDiscouragements { get; set; }
+
         IEnumerable<string> AllEncouragements { get; set; }
 
+        string GetRandomDiscouragement();
+
         string GetRandomEncouragement();
+
+        SVsServiceProvider GetServiceProvider();
     }
 }
